d71271373d44c87b3f971f2c09a1df6cc0cc7f0f
Rak <wertwerty60@gmail.com>
2023-02-05 01:54:07 +0200

    Change cmake file

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rak/Desktop/snake
-# Build directory: /home/rak/Desktop/snake/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
